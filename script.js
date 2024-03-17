9eb2b93c4056635a2f11d64fde249855c1b8c301
BreandenS <162747829+BreandenS@users.noreply.github.com>
2024-03-17 21:28:41 +0100

    I created an object for the  exercises then i printed them to the DOM element.

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
-const pushUpsExercise = {
-    name: "Push-ups",
-    tags: ["Chest", "Body mass", " Body core"],
-    description: "Push-ups are a fundamental bodyweight exercises that targets the Chest, Body Core,Biceps and Triceps. ",
-    videoLink: "https://www.example.com/push-up-video"
+
+const objectOne = {
+
+    pushUpsExercise: {
+        name: "Push-ups",
+        tags: ["Chest", "Body mass", " Body core"],
+        description: "Push-ups are a fundamental bodyweight exercises that targets the Chest, Body Core,Biceps and Triceps. ",
+        videoLink: "https://www.example.com/push-up-video"
 
 
+    },
+    pullUpsExercise: {
+        name: "Pull-ups",
+        tags: ["back", "Body mass", " Body core"],
+        description: "Pull-ups targets the back, arms and also targets the body core.",
+        videoLink: ""
+
+    },
+    squatsExercise: {
+        name: "squats",
+        tags: ["quads", "glutes", " Body core", "harmstrings"],
+        description: "Squats helps to build strong legs, strong core and also helps  creating endurance in the body by targeting almost the whole body. ",
+        videoLink: ""
+
+    },
+    deadliftsExercise: {
+        name: "deadlifts",
+        tags: ["back", "Body mass", " Body core", "harmstrings"],
+        description: "Deadlifts are exercises that are done to strenghten the back , they also target the whole body and makes you very strong. ",
+        videoLink: ""
+
+    }
 }
-const pullUpsExercise = {
-    name: "Pull-ups",
-    tags: ["back", "Body mass", " Body core"],
-    description: "Pull-ups targets the back, arms and also targets the body core.",
-    videoLink: ""
+const pushUpsDes = objectOne.pushUpsExercise.description;
+const pushUpsDesElement = document.getElementById("push-ups");
+pushUpsDesElement.innerHTML = (pushUpsDesElement);
 
-}
-const squatsExercise = {
-    name: "squats",
-    tags: ["quads", "glutes", " Body core","harmstrings"],
-    description: "Squats helps to build strong legs, strong core and also helps  creating endurance in the body by targeting almost the whole body. ",
-    videoLink: ""
+const pullUpsDes = objectOne.pullUpsExercise.description;
+const pullupsDesElement = document.getElementById("pull-ups");
+pullupsDesElement.innerHTML = (pullupsDesElement);
 
-}
-const deadliftsExercise = {
-    name: "deadlifts",
-    tags: ["back", "Body mass", " Body core", "harmstrings"],
-    description: "Deadlifts are exercises that are done to strenghten the back , they also target the whole body and makes you very strong. ",
-    videoLink: ""
+const squatsDes = objectOne.squatsExercise.description;
+const squatsDesElement = document.getElementById("squats");
+squatsDesElement.innerHTML = (squatsDesElement);
 
-}
+const deadLiftsDes = objectOne.deadliftsExercise.description;
+const deadLiftsDesElement = document.getElementById("deadlifts");
+deadLiftsDesElement.innerHTML = (deadLiftsDesElement);
 
-const exerciseElements = document.getElementById("workouts");
 
-exerciseElements.innerHTML = `${JSON.stringify(pushExercise)}`;
+
 
